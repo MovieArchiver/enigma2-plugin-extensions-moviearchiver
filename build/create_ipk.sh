53db10540d97af5907c715f528d17e67026a945a
Sebastian <mail@sebastian-wichmann.de>
2013-10-23 14:13:18 +0200

    buildscript comments

--- build/create_ipk.sh
+++ build/create_ipk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Kopiere Plugin MovieArchiver nach /tmp/temp/"
+echo "Kopiere Plugin MovieArchiver nach /tmp/tmpMovieArchiver/"
 rm -r /tmp/tmpMovieArchiver
 mkdir -p /tmp/tmpMovieArchiver/usr/lib/enigma2/python/Plugins/Extensions/MovieArchiver
 cp -r ../src/* /tmp/tmpMovieArchiver/usr/lib/enigma2/python/Plugins/Extensions/MovieArchiver
@@ -10,5 +10,4 @@ echo "Zugriffsrechte der CONTROL Scripte zur Ausfuehrung bereit machen"
 chmod 0755 /tmp/tmpMovieArchiver/CONTROL/*
 
 echo "Erstelle ipk in /tmp"
-#cd /tmp
 ipkg-build /tmp/tmpMovieArchiver /tmp
